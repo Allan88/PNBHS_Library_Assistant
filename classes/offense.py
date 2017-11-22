4daa58631893475e67f44443620b7a9b20f847ae
Allan Bailey <allanbailey88@gmail.com>
2017-11-23 01:06:06 +1300

    Reworked for much cleaner CSV parsing and UI improvements

---
+++ classes/offense.py
@@ -0,0 +1,9 @@
+class Offense:
+    def __init__(self):
+        self.classification = ""
+        self.barcode = ""
+        self.type = ""
+        self.title = ""
+        self.author = ""
+        self.dueDate = ""
+        self.fine = 0.00
